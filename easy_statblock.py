18172ebcfa2b06c1f1916ae3e72589d587e48f2c
Patrick Rhomberg @ home <purelyapplied@gmail.com>
2018-08-21 22:07:06 -0700

    Add some Django framework stubs.

--- easy_statblock.py
+++ easy_statblock.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+from functools import partial
+
 from easy_statblock import main
+from http.server import test, SimpleHTTPRequestHandler
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -10,3 +14,8 @@ if __name__ == '__main__':
     balor_vars_file = r"C:\Users\admin\PycharmProjects\stat_block_parser\easy_statblock\example\simple.yml"
     main(template_file, balor_vars_file)
 
+    handler_class = partial(SimpleHTTPRequestHandler,
+                            directory=os.getcwd())
+    print("Go do http://127.0.0.1:8080/")
+    test(HandlerClass=handler_class, port=8080, bind='')
+    input("Press ENTER to quit.")
